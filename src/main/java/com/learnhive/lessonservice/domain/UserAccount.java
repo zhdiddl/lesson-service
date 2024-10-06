ccbab6651b3de6a5f9a0c18d7e1bf8aeb3937c5f
jina <jinahoi@naver.com>
2024-10-06 21:59:46 +0900

    feat: 사용자 엔티티 수정 및 권한 부여 로직 추가
    - 로그인에 사용할 사용자명과 추가 인증을 진행하는 이메일은 unique로 설정했습니다.
    - 사용자 역할은 변경할 경우 서비스에 혼동이 예상되어, 생성자로 한 번 설정하면 변경할 수 없도록 설정했습니다.
    - 본인 이메일이 맞는지 확인하는 기능을 제공하기 위해, 이메일 인증 코드를 저장하고 인증 완료 여부를 저장하는 필드를 추가했습니다.
    - 생성자는 private으로 설정하고 정적 메소드로 객체 생성을 유도하여 잘못된 객체 생성을 하지 않도록 구현했습니다.
    - 사용자가 가입 시 Dto에서 사용자 역할을 입력 받도록 추가했습니다.
    - 사용자 역할 구분에 사용할 UserRole 엔티티를 추가했습니다.
    - Spring Security가 UserDetails에서 인증된 사용자 정보를 불러오는 과정에서 사용자 권한을 확인하고 접근을 제어할 수 있도록 UserRole을 작성했습니다.

--- src/main/java/com/learnhive/lessonservice/domain/UserAccount.java
+++ src/main/java/com/learnhive/lessonservice/domain/UserAccount.java
@@ -2,7 +2,9 @@ package com.learnhive.lessonservice.domain;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
 import org.hibernate.annotations.UpdateTimestamp;
 import org.springframework.format.annotation.DateTimeFormat;
 
@@ -10,15 +12,13 @@ import java.time.LocalDateTime;
 
 @Getter
 @Setter
-@Builder
-@AllArgsConstructor
-@NoArgsConstructor
 @Entity
 public class UserAccount extends AuditingFields {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long userId;
+    @Setter(AccessLevel.NONE)
+    private Long id;
 
     @Column(nullable = false, unique = true)
     private String username;
@@ -31,9 +31,36 @@ public class UserAccount extends AuditingFields {
     @Column(nullable = false, unique = true)
     private String email;
 
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    @Setter(AccessLevel.NONE) // 생성자로 설정 후 변경 불가
+    private UserRole userRole;
+
+    @JsonIgnore
+    private String emailVerificationCode;
+
+    @JsonIgnore
+    private LocalDateTime emailVerificationCodeExpiry;
+
+    private boolean emailVerified = false;
+
     @UpdateTimestamp
     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
     private LocalDateTime lastLogin;
 
     private boolean isDeleted = false;
+
+    protected UserAccount() {}
+
+    private UserAccount(String username, String password, String email, UserRole userRole) {
+        this.username = username;
+        this.userPassword = password;
+        this.email = email;
+        this.userRole = userRole;
+    }
+
+    public static UserAccount of(String username, String password, String email, UserRole userRole) {
+        return new UserAccount(username, password, email, userRole);
+    }
+
 }
